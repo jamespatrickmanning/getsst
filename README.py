9e514bcd010101bfbcad36f5b8b2a9859d4dadd2
Jim Manning <james.manning@noaa.gov>
2020-06-23 16:00:15 -0400

    started repository

---
+++ README.py
@@ -0,0 +1,10 @@
+Readme for "getsst"
+
+In order to make this run on my Toshiba Windows Laptop, I first needed toL
+conda config --add channels conda-forge
+conda install pydap 
+
+I also needed to change my print statements to be Python 3 compatible as "print()"
+
+
+
