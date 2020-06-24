e8f2cd48d3a457f2ede0568aa9f60f1070c72d3a
Jim Manning <james.manning@noaa.gov>
2020-06-24 10:18:03 -0400

    added code

--- README.py
+++ README.py
@@ -1,10 +1,23 @@
 Readme for "getsst"
 
-In order to make this run on my Toshiba Windows Laptop, I first needed toL
-conda config --add channels conda-forge
-conda install pydap 
+JiM's adaptation of Huimin's code from 2017
 
-I also needed to change my print statements to be Python 3 compatible as "print()"
+Note: In order to make this run on my old Toshiba Windows Laptop during 2020 Pandemic, I first needed to:
+
+    -added lots of print statements to be Python 3 compatible as "print()" to monitor progress
+    -changed the URL from basin to thredds.demac 
+    -change the "pydap" method to the more up-to-date "netCDF4"
+    -convert the masked sst array to a regular array
+    -posted on Github for Jack Polentes to try
+    
+Still not sure why it takes so long (~30 minutes) to run.
+
+This code makes as it stands makes a single png for user supplied date and time.
+Hardcodes include:
+    - number of days to aggregate images (done by UDEL folks)
+    - cont_lev to specify the min, max, and int of temp contours
+    
+
 
 
 
